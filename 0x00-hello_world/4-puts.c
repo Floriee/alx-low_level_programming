e98d69c61dbac14b30f3991f37fa2248f2aba4e3
Floriee <olayiwolaflo@gmail.com>
2022-10-13 11:03:28 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main-The entry point of c program.
+ *
+ * Return:0(successful)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+
+	return(0);
+}

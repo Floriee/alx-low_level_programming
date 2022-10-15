c7d8fadff173fde8e5dc9a3b56a69c8e8a7f5234
Floriee <olayiwolaflo@gmail.com>
2022-10-15 11:52:50 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Return:always(0)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

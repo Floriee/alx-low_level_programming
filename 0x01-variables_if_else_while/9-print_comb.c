2a1d1794b8afc3ce0b1daf652042aeceed2ea505
Floriee <olayiwolaflo@gmail.com>
2022-10-16 15:22:25 -0700

    Task 11

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int x;
 
-	for (x = '0'; x < '9'; x++)
+	for (x = '0'; x < '10'; x++)
 	{
-		putchar(x);
-		if (x != 9)
+		putchar(x + 0);
+		if (x < 9)
 		{
 		putchar(',');
 		putchar(' ');

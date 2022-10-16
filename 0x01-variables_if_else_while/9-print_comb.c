c9aa8b501def4ef590475418c78a920c086d79f6
Floriee <olayiwolaflo@gmail.com>
2022-10-16 03:02:10 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Return:always(0)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x == '0'; x <= '9'; x++)
+	putchar(x);
+
+	if (x <= '0')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

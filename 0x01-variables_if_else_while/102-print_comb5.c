cce293834afcd70b8461b3f94a181d373d22497a
Floriee <olayiwolaflo@gmail.com>
2022-10-17 15:33:32 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; x < 99; x++)
+	for (x = 0; x <= 99; x++)
 	{
-		for (y = x + 1; y < 99; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
 			putchar('0' + x / 10);
 			putchar('0' + x % 10);

0d4eb251e8c609256f8633e50b684d74309dc2f1
Floriee <olayiwolaflo@gmail.com>
2022-10-19 06:00:58 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add-Adds two int and return the result
+ * @x:an int to be added
+ * @y:an int to be added
+ * Return:Addition  of the two  int
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+

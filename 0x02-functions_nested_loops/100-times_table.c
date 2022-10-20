5ac18388a5096d704955318bf4bd6880d2c40236
Floriee <olayiwolaflo@gmail.com>
2022-10-19 19:41:05 -0700

    Task 11

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,14 +12,14 @@ void print_times_table(int n)
 	int b;
 	int c;
 
-	if (n >=0; && n <= 15)
+	if (n >= 0; &&n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			c = a * b;
 			_putchar(44);
 			_putchar(32);
-			if (c <=9)
+			if (c <= 9)
 			{
 				_putchar(32);
 				_putchar(32);
@@ -35,7 +35,7 @@ void print_times_table(int n)
 			{
 				_putchar(((c / 100) % 10) + 48);
 				_putchar(((c / 10) % 10) + 48);
-				_putchar((c % 10) +48);
+				_putchar((c % 10) + 48);
 			}
 		}
 		_putchar('\n');

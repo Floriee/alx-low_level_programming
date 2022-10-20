dc7e2a9cec493c995bd69091cbb198b808cd1a1e
Floriee <olayiwolaflo@gmail.com>
2022-10-19 18:54:04 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,17 +13,20 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
+		_putchar(48);
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(' ');
-			_putchar(',');
 			c = a * b;
+			_putchar(44);
+			_putchar(32);
 			if (c <= 9)
-				_putchar(' ');
+			{
+				_putchar(32);
+				_putchar(c + 48)
+			}
 			else
-				_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
 	}

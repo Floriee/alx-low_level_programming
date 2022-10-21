fffe359673060e9b01afda8934318ee1f03e3e25
Floriee <olayiwolaflo@gmail.com>
2022-10-21 03:02:58 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints the numbers from 0 to 9
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar("\n");
+	return (0);
+}

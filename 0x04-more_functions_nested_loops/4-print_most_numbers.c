61314283fd09d310b8686736e71e2f930efe3188
Floriee <olayiwolaflo@gmail.com>
2022-10-21 03:22:10 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- prints numbers from 0 to 8
+ * (neither 2 0r 9)
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; c < 58; c++)
+	{
+		if (ch != 50 && c != 52)
+			_putchar(ch);
+	}
+	_putchar("\n");
+}
